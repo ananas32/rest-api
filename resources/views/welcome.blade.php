30723514b0bd1ac055738ee1d26b29d22cdfda7d
Vitalik Loshniy <porsche839@gmail.com>
2020-06-26 23:56:47 +0300

    user list with pagination

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,141 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+<script>
+    getContent('/api/users/');
+
+    $("body").on('click', '.page-link', function () {
+        getContent('/api/users?page='+$(this).text());
+    });
+
+    function getContent(url) {
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (response) {
+                var data = response.data;
+                var html = renderTable(data);
+                $('#task-list').html(html);
+                var html = renderPagination(response.last_page, response.path, response.current_page);
+                $('#task-pagination').html(html);
+            },
+            error: function (response) {
+                console.log(response);
             }
+        });
+    }
 
-            .full-height {
-                height: 100vh;
+    function renderTable(data) {
+        var html = '';
+        for (var item in data) {
+            var obj = data[item];
+            html = html + '<tr>\n' +
+                '<td>' + obj.user_id + '</td>\n' +
+                '<td>' + obj.first_name + '</td>\n' +
+                '<td>' + obj.last_name + '</td>\n' +
+                '<td>' + obj.email + '</td>\n' +
+                '<td>' + obj.created_at + '</td>\n' +
+                '<td>\n' +
+                '<button type="button" class="btn btn-warning">Show</button>\n' +
+                '<button type="button" class="btn btn-info">Edit</button>\n' +
+                '<button type="button" class="btn btn-danger">Delete</button>\n' +
+                '</td>\n' +
+                '</tr>';
+        }
+        return html;
+    }
+
+    function renderPagination(lastPage, path, currentPage) {
+        var html = '';
+        for (var i = 1; i <= lastPage; i++) {
+            var activeClass = '';
+            if(i === currentPage) {
+                activeClass = 'active';
             }
+            html = html + '<li class="page-item '+activeClass+'"><a class="page-link" href="#">' + i + '</a></li>';
+        }
+        return html;
+    }
+</script>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div class="container">
+    <h2>Table Head Colors</h2>
+    <p>The .thead-dark class adds a black background to table headers, and the .thead-light class adds a grey background
+        to table headers:</p>
+    <table class="table">
+        <thead class="thead-dark">
+        <tr>
+            <th>Id</th>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Created at</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody id="task-list">
+        </tbody>
+    </table>
+    <ul class="pagination" id="task-pagination">
+        {{--        <li class="page-item"><a class="page-link" href="#">1</a></li>--}}
+        {{--        <li class="page-item active"><a class="page-link" href="#">2</a></li>--}}
+        {{--        <li class="page-item"><a class="page-link" href="#">3</a></li>--}}
+    </ul>
+    {{--    <table class="table">--}}
+    {{--        <thead class="thead-light">--}}
+    {{--        <tr>--}}
+    {{--            <th>Firstname</th>--}}
+    {{--            <th>Lastname</th>--}}
+    {{--            <th>Email</th>--}}
+    {{--        </tr>--}}
+    {{--        </thead>--}}
+    {{--        <tbody>--}}
+    {{--        <tr>--}}
+    {{--            <td>John</td>--}}
+    {{--            <td>Doe</td>--}}
+    {{--            <td>john@example.com</td>--}}
+    {{--        </tr>--}}
+    {{--        <tr>--}}
+    {{--            <td>Mary</td>--}}
+    {{--            <td>Moe</td>--}}
+    {{--            <td>mary@example.com</td>--}}
+    {{--        </tr>--}}
+    {{--        <tr>--}}
+    {{--            <td>July</td>--}}
+    {{--            <td>Dooley</td>--}}
+    {{--            <td>july@example.com</td>--}}
+    {{--        </tr>--}}
+    {{--        </tbody>--}}
+    {{--    </table>--}}
+</div>
 
-            .position-ref {
-                position: relative;
-            }
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
         </div>
-    </body>
+    @endif
+
+</div>
+</body>
 </html>
